e2d1b9ffcb3396570247c17c024420f75793c4d9
Joshua Keegan <37237625+JoshuaKeegan3@users.noreply.github.com>
2022-11-13 17:54:36 +1300

    Add files via upload

---
+++ Secret Santa Pairer.py
@@ -0,0 +1,18 @@
+# Secret Santa Pairer
+
+# Pairs people (strings) from a pool
+# People can not be paired with themselves
+
+# Imports
+from random import shuffle
+
+# Define randomised partisipants
+pool = ["Joel", "Jack", "Addison", "Peoploe", "Phil", "Quinn", "Rex"]
+shuffle(pool)
+
+# Copy the partisipants and rotate the array
+pool2 = pool.copy()
+pool2.append(pool2.pop(0))
+
+# Ouput
+print(*zip(pool,pool2))
