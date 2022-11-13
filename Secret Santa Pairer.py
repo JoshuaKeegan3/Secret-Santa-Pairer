90c15243ae67a84fa95ebc4d5fb1cc69530bfaaf
Joshua Keegan <37237625+JoshuaKeegan3@users.noreply.github.com>
2022-11-13 18:06:02 +1300

    Update Secret Santa Pairer.py

--- Secret Santa Pairer.py
+++ Secret Santa Pairer.py
@@ -6,8 +6,8 @@
 # Imports
 from random import shuffle
 
-# Define randomised partisipants
-pool = ["Joel", "Jack", "Addison", "Peoploe", "Phil", "Quinn", "Rex", "Ashan", "Max"]
+# Define randomised participants
+pool = ["Joel", "Jack", "Addison", "Peploe", "Phil", "Quinn", "Rex", "Ashan", "Max"]
 shuffle(pool)
 
 # Copy the partisipants and rotate the array
